941e5c3ab8d9b8f345ddb6cbcc0a44d0043696c0
Gustavo <gstonin95@gmail.com>
2020-07-27 03:31:40 +0100

    toggle classes, shows description

--- scripts/DOM-handling/expandTask.js
+++ scripts/DOM-handling/expandTask.js
@@ -2,11 +2,9 @@
 
 function expandTask(e){
 
-    console.log(e.target.parentNode);
-
-    // TODO - expand the task to show, full date (dd/mm/yyyy), description and title on top
-    // make a class and hide the details with display: none, then just toggle class
-
+    e.target.parentNode.parentNode.lastChild.classList.toggle('hideElement');
+    e.target.parentNode.parentNode.firstChild.classList.toggle('activeTask');
+    
 }
 
 export { expandTask }
