168e4610c0a2730fcb86d3150b3b9ae9642ef14d
Gustavo <gstonin95@gmail.com>
2020-07-29 20:09:43 +0100

    UI change

--- scripts/DOM-handling/taskHandling/expandTask.js
+++ scripts/DOM-handling/taskHandling/expandTask.js
@@ -2,8 +2,10 @@
 
 function expandTask(e){
 
-    e.target.parentNode.parentNode.lastChild.classList.toggle('hideElement');
-    e.target.parentNode.parentNode.firstChild.classList.toggle('activeTask');
+    e.target.parentNode.parentNode.lastChild.classList.toggle('hideElement');           //targets task description
+    e.target.parentNode.parentNode.firstChild.classList.toggle('activeTask');           //targets task Div
+    e.target.parentNode.childNodes[4].classList.toggle('hideElement');                  //targets edit button
+    e.target.parentNode.childNodes[5].classList.toggle('hideElement');                  //targets remove button
     
 }
 
