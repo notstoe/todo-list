7bf6fad61c7f1138c4c39fe8adc96053881c2606
Gustavo <gstonin95@gmail.com>
2020-07-31 20:39:47 +0100

    button for adding new tabs working (DOM only) and styled properly

--- scripts/DOMhandling/modals/newTabModal.js
+++ scripts/DOMhandling/modals/newTabModal.js
@@ -1,7 +1,9 @@
+import { createTab } from '../tabHandling/createTab'
+
 function newTabModal(){
 
     const newTabModal = document.createElement('div');
-        newTabModal.classList.add('modal', 'hideElement');                          //conserving classes for styling
+        newTabModal.classList.add('modal', 'hideElement', 'tabModal');                          //conserving classes for styling
         newTabModal.setAttribute('id', 'newTabModal');
 
         const titleContainer = document.createElement('div');
@@ -9,7 +11,7 @@ function newTabModal(){
 
             const title = document.createElement('h1');
                 title.classList.add('taskTitle');
-                title.textContent = 'Create a New Task';
+                title.textContent = 'Create a new category';
             const closeBtn = document.createElement('span');
                 closeBtn.classList.add('closeModal');
                 closeBtn.textContent = '\u00D7';
@@ -17,40 +19,31 @@ function newTabModal(){
             titleContainer.append(title, closeBtn);
 
         const labelTitle = document.createElement('label');
-            labelTitle.setAttribute('for', 'inputTitle');
-            labelTitle.textContent = 'Title:';
+            labelTitle.setAttribute('for', 'inputTitleCategory');
+            labelTitle.textContent = 'Category Name:';
             labelTitle.classList.add('modalLabel');
 
         const inputTitle = document.createElement('input');
             inputTitle.setAttribute('type', 'text');
-            inputTitle.setAttribute('name', 'inputTitle');
-            inputTitle.classList.add('inputTitle');
+            inputTitle.setAttribute('name', 'inputTitleCategory');
+            inputTitle.classList.add('inputTitle', 'inputTitleCategory');
 
-        const labelDate = document.createElement('label');
-            labelDate.setAttribute('for', 'inputDate');
-            labelDate.textContent = 'Due Date:';
-            labelDate.classList.add('modalLabel');
+        const labelColor = document.createElement('label');
+            labelColor.setAttribute('for', 'inputColorCategory');
+            labelColor.textContent = 'Category Color Tab:';
+            labelColor.classList.add('modalLabel');
 
-        const inputDate = document.createElement('input');
-            inputDate.setAttribute('type', 'date');
-            inputDate.setAttribute('name', 'inputDate');
-            inputDate.classList.add('inputDate');
-            inputDate.attributes.required = '';
-
-        const labelDescript = document.createElement('label');
-            labelDescript.setAttribute('for', 'inputDescript');
-            labelDescript.textContent = 'Task Description:';
-            labelDescript.classList.add('modalLabel');
-
-        const inputDescript = document.createElement('textarea');
-            inputDescript.setAttribute('name', 'inputDescript');
-            inputDescript.classList.add('inputDescript');
+        const inputColor = document.createElement('input');
+            inputColor.setAttribute('type', 'color');
+            inputColor.setAttribute('name', 'inputColorCategory');
+            inputColor.classList.add('inputDescript', 'inputColorCategory');
+            inputColor.value = '#464658';
 
         const submitBtn = document.createElement('button');
             submitBtn.classList.add('submitBtn');
-            submitBtn.textContent = 'Add New Task';
+            submitBtn.textContent = 'Add New Category';
 
-        newTabModal.append(titleContainer, labelTitle, inputTitle, labelDate, inputDate, labelDescript, inputDescript, submitBtn);
+        newTabModal.append(titleContainer, labelTitle, inputTitle, labelColor, inputColor, submitBtn);
 
     const newTabModalOverlay = document.createElement('div');
         newTabModalOverlay.classList.add('modalOverlay', 'hideElement');
@@ -62,28 +55,20 @@ function newTabModal(){
 
         window.addEventListener('click', (e) => {if (e.target == newTabModalOverlay) toggleNewTabModal(); });
 
-        // submitBtn.addEventListener('click', () => {
+        submitBtn.addEventListener('click', () => {
 
-        //     if (inputDate.value === '' || inputTitle.value === '') {
+            if (inputTitle.value === '') {
+                alert ('Ops, looks like you forgot to add a Title!');
+                return;
+            }
 
-        //         alert ('Ops, looks like you forgot to add a Title and/or a due Date');
-        //         return;
-        //     }
-
-        //     let dateArr = inputDate.value.split('-');                                  //date as string: yyyy-mm-dd into [yyyy, mm, dd]
-
-        //     let dateFormated = format(new Date(dateArr[0], dateArr[1]-1, dateArr[2]), 'dd-MM-yyyy');
-
-            
-        //     createTask(dateFormated.split('-').join('/'), inputTitle.value, inputDescript.value);
+            createTab(inputColor.value, inputTitle.value);
         
-        //     inputDate.value = '';
-        //     inputTitle.value = '';
-        //     inputDescript.value = '';
+            inputTitle.value = '';
 
-        //     toggleNewTabModal();
+            toggleNewTabModal();
 
-        // });
+        });
 
 
     document.body.append(newTabModal, newTabModalOverlay);
