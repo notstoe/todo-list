532d15f089e34d0e3bb23b70c09615ea9da515df
Gustavo <gstonin95@gmail.com>
2020-07-31 17:20:06 +0100

    UI change

--- scripts/DOM-handling/taskHandling/expandTask.js
+++ scripts/DOM-handling/taskHandling/expandTask.js
@@ -2,11 +2,16 @@
 
 function expandTask(e){
 
-    e.target.parentNode.parentNode.lastChild.classList.toggle('hideElement');           //targets task description
+    const editBtn = e.target.parentNode.childNodes[4];
+    const rmvBtn = e.target.parentNode.childNodes[5];
+    const taskDescript = e.target.parentNode.parentNode.lastChild;
+    const checkBox = e.target.parentNode.childNodes[0].firstChild;
+
+    taskDescript.classList.toggle('hideElement');                                       
     e.target.parentNode.parentNode.firstChild.classList.toggle('activeTask');           //targets task Div
-    e.target.parentNode.childNodes[4].classList.toggle('hideElement');                  //targets edit button
-    e.target.parentNode.childNodes[5].classList.toggle('hideElement');                  //targets remove button
-    
+    editBtn.classList.toggle('hideElement');  
+
+    if (!checkBox.checked) rmvBtn.classList.toggle('hideElement');
 }
 
 export { expandTask }
--- scripts/DOM-handling/taskHandling/markTaskDone.js
+++ scripts/DOM-handling/taskHandling/markTaskDone.js
@@ -1,6 +1,12 @@
 function markTaskDone(e) {
 
-    e.target.parentNode.parentNode.parentNode.classList.toggle('taskDone');
+    const rmvBtn = e.target.parentNode.parentNode.lastChild;
+    const editBtn = e.target.parentNode.parentNode.childNodes[4];
+    const taskDescript = e.target.parentNode.parentNode.parentNode.lastChild;
+
+    e.target.parentNode.parentNode.parentNode.classList.toggle('taskDone');                     //targets task big Div
+
+    if (taskDescript.classList.contains('hideElement')) rmvBtn.classList.toggle('hideElement');
 }
 
 export { markTaskDone }
