7f4d83e739197d9960b169b00f945a8676dc0bce
Gustavo <gstonin95@gmail.com>
2020-08-10 20:27:55 +0100

    remove tab working for DOM

--- scripts/DOMhandling/tabHandling/removeTab.js
+++ scripts/DOMhandling/tabHandling/removeTab.js
@@ -1,15 +1,38 @@
+import { renderTab } from '../renderTab'
+
 function removeTab() {
-
-    // TODO - update to return to default tab
-
-    const containerTabs = document.querySelector('.containerTabs');
 
     const tabs = document.querySelectorAll('.tab');
     
+    let breakForEachLoop = false;
+
     tabs.forEach((tab) => {
 
-        if (tab.classList.contains('activeTab'))  containerTabs.removeChild(tab);
+        if (!breakForEachLoop) {
+            if (tab.classList.contains('activeTab')) {
 
+                if (tab.parentNode.children.length == 3) {                                      //stops the user from deleting all categories/tabs
+                    alert("Oops, you can't remove all categories.");
+
+                } else {
+                      
+                    if (tab.previousSibling.id === 'editRmvDiv') {                                  //if its the first tab being deleted, render the next tab
+                        
+                        tab.nextSibling.classList.add('activeTab');
+                        renderTab(0, tab.nextSibling);
+
+                    } else {
+
+                        tab.previousSibling.classList.add('activeTab');
+                        renderTab(0, tab.previousSibling);
+                    }
+                    
+                    tab.parentNode.removeChild(tab);
+                }
+
+                breakForEachLoop = true;
+            }
+        }
     });
 }
 
